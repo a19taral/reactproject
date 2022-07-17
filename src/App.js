584168392eeec912fec9bddb1d35969788114e17
Tarik Al Tayar <altayartarik@gmail.com>
2022-07-17 10:30:30 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@ import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
 import "./App.css";
+// import Particles from "react-particles-js";
 
-function App() {
-  //render();
+// const particlesOption = {
+//   particles: {
+//     line_linked: {
+//       shadow: {
+//         enable: true,
+//         color: "#3CA9D1",
+//         blur: 5
+//       }
+//     }
+//   }
+// };
 
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm /> 
-      {/* 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <Particles className="particles" params={particlesOption} /> */}
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* 
     <FaceRecognition /> */}
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,15 @@
 import React from "react";
-
+import "./ImageLinkForm.css";
 const ImageLinkForm = () => {
   return (
     <div>
       <p className="f3">{"This App detect faces in you pictures"}</p>
       <div className="center">
-        <div className="center pa4 br3">
-
-        <input className="f4 pa2 w-70 center" type="tex" />
-        <button className="w-30 grow f4 link ph3 pv2 white bh-light">Detect</button>
+        <div className="form center pa4 br3 shadow-5">
+          <input className="f4 pa2 w-70 center" type="tex" />
+          <button className="w-30 grow f4 link ph3 pv2 white bg-light-purple">
+            Detect
+          </button>
         </div>
       </div>
     </div>
