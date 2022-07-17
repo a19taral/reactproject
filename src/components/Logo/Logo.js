aa8f71adbe8bb47b03df8a56b3af26d36d892c4d
Tarik Al Tayar <altayartarik@gmail.com>
2022-07-17 07:31:27 +0200

    added button and form

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Tilt from "react-tilt";
+import logoicon from "./logoicon.png";
 import "./Logo.css";
 
 const Logo = () => {
   return (
-    <div>
+    <div className="ma4 mt0">
       <Tilt
         className="Tilt br2 shadow-2"
         options={{ max: 55 }}
         style={{ height: 150, width: 150 }}
       >
-        <div className="Tilt-inner"> 👽 </div>
+        <div className="Tilt-inner">
+          <img alt="logo" src={logoicon} />
+        </div>
       </Tilt>
     </div>
   );
