0622df58a16a047521132689b4f95460cb6a9fc0
Tarik Al Tayar <altayartarik@gmail.com>
2022-07-17 03:55:15 +0200

    first touches

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
 import "./App.css";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  //render();
+  {
+    return (
+      <div className="App">
+        <Navigation />
+
+        {/* 
+    <Logo />
+    <ImageLinkForm /> 
+    <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
